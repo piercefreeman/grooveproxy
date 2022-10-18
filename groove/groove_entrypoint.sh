bf9d4415097eda3ff70dd2403801036612e20289
Pierce Freeman <piercefreeman@gmail.com>
2022-10-18 09:41:22 -0700

    Expand docker entrypoint to publish wheel

--- groove/groove_entrypoint.sh
+++ groove/groove_entrypoint.sh
@@ -2,6 +2,11 @@
 
 if [ "$1" = "test-python" ]; then
     cd groove-python && exec poetry run pytest -s groove/tests "${@:2}"
+elif [ "$1" = "deploy-python" ]; then
+    cp -r proxy groove-python
+    cd groove-python
+    poetry build
+    poetry publish --username $PYPI_USERNAME --password $PYPI_PASSWORD
 else
     exec "$@"
 fi
