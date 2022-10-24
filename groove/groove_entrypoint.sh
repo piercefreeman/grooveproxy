a82b6e00d1f2d642d33e40173d32431efce38a5f
Pierce Freeman <piercefreeman@gmail.com>
2022-10-24 06:08:34 -0700

    Add go test runner

--- groove/groove_entrypoint.sh
+++ groove/groove_entrypoint.sh
@@ -4,6 +4,8 @@ if [ "$1" = "test-python" ]; then
     cd groove-python && exec poetry run pytest -s groove/tests "${@:2}"
 elif [ "$1" = "test-node" ]; then
     cd groove-node && exec npm run test "${@:2}"
+elif [ "$1" = "test-go" ]; then
+    cd proxy && exec go test "${@:2}"
 else
     exec "$@"
 fi
